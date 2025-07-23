e332ed7384d9ff0151951822151aa2f6cf89c7d4
John Donovan <john@geospark.co.uk>
2025-07-23 14:38:23 +0100

    Tidied things up a bit

---
+++ README.md
@@ -0,0 +1,2 @@
+Takes some sample data from EOenergy of solar irradiation of a potential solar farm every 30 minutes for a year,
+and outputs a parquet file indexed by timestamp.
--- main.py
+++ main.py
@@ -1,30 +1,38 @@
-import pathlib
+from pathlib import Path
 
 import requests
 import pandas as pd
 import fastparquet as fp
 
 
-def download_csv():
+def download_csv(base_file_name: str):
     base_url = "https://storage.googleapis.com/eo_best_practices_2023/EOenergy"
 
     for m in range(1, 13):
-        # file_name = f"LST_analysis_above_solar_irradiation_threshold_2024_{m:02}.csv"
-        file_name = f"lst_DNI_monthly_data_2024_{m:02}.csv"
+        file_name = f"{base_file_name}_{m:02}.csv"
+
+        output_file_path = Path(f"source_data/{base_file_name}/{file_name}")
+        if output_file_path.exists():
+            print(f"File {output_file_path} already exists. Skipping.")
+            continue
+
         url = f"{base_url}/{file_name}"
         r = requests.get(url)
 
         if r.status_code != 200:
+            print(f"Request failed with status code {r.status_code}. Skipping.")
             continue
 
-        with open(f"source_data/lst_DNI_monthly_data_2024/{file_name}", "wt") as f:
+        with open(output_file_path, "wt") as f:
+            print(f"Writing to {output_file_path}")
             f.write(r.text)
 
 
-def csv_to_parquet():
+def csv_to_parquet(base_file_name: str):
     dfs = []
 
-    for file_name in pathlib.Path("source_data/LST_analysis_above_solar_irradiation_threshold_2024").glob("*.csv"):
+    for file_name in Path(f"source_data/{base_file_name}").glob("*.csv"):
+        print(f"Reading {file_name}")
         dfs.append(pd.read_csv(
             file_name,
             index_col=[0],
@@ -35,9 +43,20 @@ def csv_to_parquet():
         ))
 
     df = pd.concat(dfs).sort_index()
-    fp.write("output/LST_analysis_above_solar_irradiation_threshold_2024.parquet", df)
+    output_file_name = f"output/{base_file_name}.parquet"
+    print(f"Writing to {output_file_name}")
+    fp.write(output_file_name, df)
+
+
+def main():
+    base_file_name = "lst_DNI_monthly_data_2024"
+    download_csv(base_file_name)
+    csv_to_parquet(base_file_name)
+
+    base_file_name = "LST_analysis_above_solar_irradiation_threshold_2024"
+    download_csv(base_file_name)
+    csv_to_parquet(base_file_name)
 
 
 if __name__ == "__main__":
-    # download_csv()
-    csv_to_parquet()
+    main()
